04dbfc8923c9bcc62acb69b0eb343851ba4248da
kimmyoungji <59907325+kimmyoungji@users.noreply.github.com>
2022-09-30 10:13:15 +0900

    navigation and routing

---
+++ test01/src/routes/page2/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {num1,num2,num3} from "../../store/store.ts";
+    import {onDestroy} from 'svelte';    
+    // let countValue:number;
+    // const unsubscribe =  num1.subscribe((val)=>{
+    //     countValue = val;
+    // });
+
+    // num1.update( n => n + 100 );
+
+    // onDestroy(unsubscribe);
+    $num1 = $num1 + 100;
+    // $num2 = 100; 
+</script>
+
+<h1>num1: {$num1}</h1>
+<h1>num2: {$num2}</h1>
+<h1>num3: {$num3}</h1>
